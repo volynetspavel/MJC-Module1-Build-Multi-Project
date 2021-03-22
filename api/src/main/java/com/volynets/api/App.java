a9ecbcb296dd0d82640e50d401a5d90e83b3ed60
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-03-22 16:44:02 +0300

    add main class for running app

---
+++ api/src/main/java/com/volynets/api/App.java
@@ -0,0 +1,12 @@
+package com.volynets.api;
+
+import com.volynets.core.Utils;
+
+/**
+ * Class for running app.
+ */
+public class App {
+    public static void main(String[] args) {
+        Utils.isAllPositiveNumbers("12", "79");
+    }
+}
